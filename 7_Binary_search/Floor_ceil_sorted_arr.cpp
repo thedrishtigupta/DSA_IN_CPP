49578053024e59b0d3fe4a06e9d3a79135de4a77
Drishti Gupta <thedrishtigupta@gmail.com>
2025-07-25 16:31:31 +0530

    BS: Floor ceil

---
+++ 7_Binary_search/Floor_ceil_sorted_arr.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> floor_ceil(vector<int>& arr, int x) {
+    int n = arr.size();
+
+    int i = 0, j = n-1;
+    vector<int> ans(2, -1);
+
+    while (i <= j) {
+        int m = i + ((j-i)/2);
+        if (arr[m] == x) {
+            ans[0] = x, ans[1] = x; break;
+        } else if (arr[m] > x) {
+            ans[1] = arr[m];
+            j = m-1;
+        } else {
+            ans[0] = arr[m];
+            i = m+1;
+        }
+    }
+    return ans;
+}
+
+void print(vector<int> arr) {
+    for (int val : arr) cout<<val<<"\t";
+    cout<<endl;
+}
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 8, 10};
+    int x;
+    cin>>x;
+
+    print(floor_ceil(arr, x));
+    return 0;
+}
