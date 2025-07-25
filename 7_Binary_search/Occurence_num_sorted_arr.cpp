56b502629fc7372a5b3422902c9714ece83bec75
Drishti Gupta <thedrishtigupta@gmail.com>
2025-07-25 17:17:12 +0530

    BS : Occurence of a number

---
+++ 7_Binary_search/Occurence_num_sorted_arr.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int lower_bound(vector<int>& arr, int x) {
+    int n = arr.size();
+    int i = 0, j = n-1, ans = n;
+    while (i <= j) {
+        int m = i + ((j-i)/2);
+        if (x > arr[m]) i = m+1;
+        else if (x <= arr[m]) {
+            j = m -1;
+            ans = m;
+            if (arr[m-1] != x) break;
+        }
+    }
+    return ans;
+}
+int upper_bound(vector<int>& arr, int x) {
+    int n = arr.size();
+    int i = 0, j = n-1, ans = n;
+    while (i <= j) {
+        int m = i + ((j-i)/2);
+        if (x >= arr[m]) i = m+1;
+        else if (x < arr[m]) {
+            j = m - 1;
+            ans = m;
+        }
+    }
+    return ans;
+}
+int main() {
+    vector<int> arr = {1, 2, 2, 2, 2, 3, 3 ,3 , 4, 5};
+    int x;
+    cin>>x;
+
+    cout<<upper_bound(arr, x)-lower_bound(arr, x)<<endl;
+    return 0;
+}
