7f7cdaa1f6659d492655683c1210662a56029e05
Drishti Gupta <thedrishtigupta@gmail.com>
2025-09-20 11:31:53 +0530

    Binary trees

---
+++ 10_Binary_trees/build_tree.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node() : data(0), left(NULL), right(NULL)
+    { }
+
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+    Node (int data, Node* left, Node* right) {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
+class Pair {
+    public:
+    int height;
+    int dia;
+};
+
+
+Node* buildTree() {
+    int d;
+    cin>>d;
+    if (d == -1) return NULL;
+
+    Node* root = new Node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+void PreOrder(Node* root) {
+    if(!root) return;
+    cout<<root->data<<" ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+
+void InOrder(Node* root) {
+    if(!root) return;
+    InOrder(root->left);
+    cout<<root->data<<" ";
+    InOrder(root->right);
+}
+
+void PostOrder(Node* root) {
+    if(!root) return;
+    PostOrder(root->left);
+    PostOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+int countNodes(Node* root) {
+    if(!root) return 0;
+    return countNodes(root->left) + countNodes(root->right) + 1;
+}
+
+int heightTree(Node* root) {
+    if(!root) return 0;
+
+    return max(heightTree(root->left), heightTree(root->right))+1;
+}
+
+int diameterTree(Node* root) {
+    if(!root) return 0;
+
+    int op1 = heightTree(root->left) + heightTree(root->right); // max diameter via root node
+    int op2 = diameterTree(root->left); // LST ka maximum diameter
+    int op3 = diameterTree(root->right); // RST ka maximum diameter
+
+    return max(op1, max(op2, op3));
+}
+
+Pair fastDiameter(Node* root) {
+    Pair p;
+    if (!root) {
+        p.height = p.dia = 0;
+        return p;
+    }
+
+    Pair left = fastDiameter(root->left);
+    Pair right = fastDiameter(root->right);
+
+    p.height = max(left.height, right.height) +1;
+
+    int op1 = left.height + right.height;
+    int op2 = left.dia;
+    int op3 = right.dia;
+
+    p.dia = max(op1, max(op2, op3));
+
+    return p;
+}
+// 8 10 1 -1 -1 6 4 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+int main() {
+    cout<<"Enter input: ";
+    
+    Node* root = buildTree();
+    cout<<"Total nodes = "<<countNodes(root)<<endl;
+    cout<<"Height of tree = "<<heightTree(root)<<endl;
+    cout<<"Diameter of tree = "<<diameterTree(root)<<endl;
+    Pair p = fastDiameter(root);
+    cout<<"Fast height= "<<p.height<<endl;
+    cout<<"Fast diameter= "<<p.dia<<endl;
+    cout<<"Preorder:";
+    PreOrder(root);
+    cout<<endl;
+    cout<<"Inorder: ";
+    InOrder(root);
+    cout<<endl;
+    cout<<"Postorder: ";
+    PostOrder(root);
+    cout<<endl;
+    return 0;
+}
