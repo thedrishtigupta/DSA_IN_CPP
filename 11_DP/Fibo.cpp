a9c4fc1c027e192eadf247c2c35c2c7bedc13ae2
Drishti Gupta <thedrishtigupta@gmail.com>
2025-10-04 22:54:31 +0530

    DP: Top-down and Bottom-up approach

--- 11_DP/Fibo.cpp
+++ 11_DP/Fibo.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-int solve (int n, vector<int>& dp) {
+int topDown (int n, vector<int>& dp) {
     if(n == 0 || n == 1) {
         dp[n] = n;
         return n;
@@ -10,15 +10,26 @@ int solve (int n, vector<int>& dp) {
 
     if (dp[n] != -1) return dp[n];
 
-    return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    return dp[n] = topDown(n-1, dp) + topDown(n-2, dp);
+}
+
+int bottomUp(int n) {
+    vector<int> dp(n+1); // +1 is important!!
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+
+    return dp[n];
 }
 int main() {
     int n; cin>>n;
     vector<int> dp(n+1, -1);
 
-    for(int i = 0; i <= n; i++) cout<<dp[i]<<" ";
+    // for(int i = 0; i <= n; i++) cout<<dp[i]<<" ";
 
-    cout<<endl<<solve(n, dp)<<endl;;
-    for(int i = 0; i <= n; i++) cout<<dp[i]<<" ";
+    cout<<endl<<topDown(n, dp)<<endl;;
+    //for(int i = 0; i <= n; i++) cout<<dp[i]<<" ";
+    cout<<bottomUp(n)<<endl;
     return 0;
 }
