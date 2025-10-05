c8e2fb45d83924da1809635e1fef3e6c00777e89
Drishti Gupta <thedrishtigupta@gmail.com>
2025-10-05 11:52:21 +0530

    LIS using DP O(N^2)

--- 11_DP/LIS.cpp
+++ 11_DP/LIS.cpp
@@ -6,12 +6,16 @@ using namespace std;
 int bottomUp(vector<int>& arr, int n) {
     vector<int> dp(n, 1);
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i < n; i++) {
         for(int j = n-1; j >= 0; j--) {
             if(arr[i] > arr[j]) dp[i] = max(dp[i], dp[j] +1);
         }
     }
-    return dp[n-1];
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans = max(ans, dp[i]);
+    }
+    return ans;
 }
 int main() {
     int n; cin>>n;
