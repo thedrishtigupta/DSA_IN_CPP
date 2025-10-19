198ce6d6a6f2abab5ff4fc59f574adaa38d2d645
Drishti Gupta <thedrishtigupta@gmail.com>
2025-10-19 12:14:13 +0530

    Disjoint set union

--- 13_Disjoint_Set_Union/DSUclass.cpp
+++ 13_Disjoint_Set_Union/DSUclass.cpp
@@ -5,12 +5,14 @@ class DSU {
     public:
     int* parent;
     int n;
+    int* rank;
 
     DSU(int N) {
         n = N+1;
         parent = new int[N+1];
+        rank = new int[N+1];
 
-        for(int i = 0; i <=N; i++) parent[i] = i;
+        for(int i = 0; i <=N; i++) {parent[i] = i; rank[i] = 1;}
     }
 
     int get_leader(int u) { // gives leader
@@ -23,7 +25,13 @@ class DSU {
         int leader_u = get_leader(u);
         int leader_v = get_leader(v);
 
-        if(leader_u != leader_v) parent[leader_v] = leader_u;
+        if(leader_u != leader_v) {
+            if(rank[leader_u] < rank[leader_v]) swap(leader_u, leader_v);
+
+            parent[leader_v] = leader_u;
+
+            if(rank[leader_u] == rank[leader_v]) rank[leader_u]++;
+        }
     }
 };
 int main() {
