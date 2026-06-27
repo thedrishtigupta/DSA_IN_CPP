eb84bcc56cb8018cb6b4d87d3b0630147cc8ee30
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-27 23:25:16 +0530

    DP : Fibonacci number

---
+++ 11_DP/509_Fibonacci_number.cpp
@@ -0,0 +1,19 @@
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        int c;
+        int a = 0, b = 1;
+
+        for(int i = 1; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+};
