827af95b4ce7fd9ecb0e9bbc67305970225fb6bb
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-28 21:14:44 +0530

    DP: House robber 2 with constant space

--- 11_DP/213_House_Robber_II.cpp
+++ 11_DP/213_House_Robber_II.cpp
@@ -29,3 +29,32 @@ public:
         return max(op1, op2);
     }
 };
+
+//Constant space
+class Solution {
+public:
+    int solve(vector<int>& nums, int l, int r) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i = l; i <= r; i++) {
+            int cur = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+
+        return max(
+            solve(nums, 0, n - 2),
+            solve(nums, 1, n - 1)
+        );
+
+    }
+};
