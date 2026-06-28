1eacbd85fd8358b651b51f72ea6f537a745faf45
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-28 18:55:00 +0530

    DP: House robber, bottom up

--- 11_DP/198_House_robber.cpp
+++ 11_DP/198_House_robber.cpp
@@ -19,3 +19,26 @@ public:
         return solve(nums, 0);
     }
 };
+
+
+// Bottom up
+/*
+steal = nums[i-1] + t[i-2]
+skip = t[i-1]
+*/
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> t(n+1, 0);
+
+        t[1] = nums[0];
+
+        for(int i = 2; i <= n; i++) {
+            t[i] = max(nums[i-1] + t[i-2], t[i-1]);
+        }
+
+        return t[n];
+    }
+};
