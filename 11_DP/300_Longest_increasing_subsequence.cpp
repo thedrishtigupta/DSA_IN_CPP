53475dd6a174dee3a00cba156f7938782f2d895d
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-29 19:24:45 +0530

    DP: Longest increasing subsequence(LIS) usinf recursion + memoization

---
+++ 11_DP/300_Longest_increasing_subsequence.cpp
@@ -0,0 +1,25 @@
+
+
+//Recursion + memoization
+class Solution {
+public:
+    int t[2501][2502];
+    int solve(vector<int>& nums, int& n, int i, int prev) {
+        if(i >= n) return 0;
+
+        if(t[i][prev+1] != -1) return t[i][prev+1];
+        int op1 = 0;
+        if (prev == -1 || nums[i] > nums[prev]) op1 = 1 + solve(nums, n, i+1, i);
+
+        int op2 = solve(nums, n, i+1, prev);
+
+        return t[i][prev+1] = max(op1, op2);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        memset(t, -1, sizeof(t));
+        return solve(nums, n, 0, -1);
+    }
+};
+
