9da3da8c7c2df0e656d636ffd8ad7a7c637af5fe
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-29 19:43:35 +0530

    DP: Longest increasing subsequence, bottom up approach

--- 11_DP/300_Longest_increasing_subsequence.cpp
+++ 11_DP/300_Longest_increasing_subsequence.cpp
@@ -23,3 +23,23 @@ public:
     }
 };
 
+//Bottom up
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> t(n, 1);
+
+        int ans = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    t[i] = max(t[i], t[j]+1);
+                }
+            }
+            ans = max(ans, t[i]);
+        }
+
+        return ans;
+    }
+};
