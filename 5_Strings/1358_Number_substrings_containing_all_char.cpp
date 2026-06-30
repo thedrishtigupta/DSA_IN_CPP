b7ebcf9f45f06348bf63bf070df7808c8d71f463
Drishti Gupta <thedrishtigupta@gmail.com>
2026-06-30 21:33:01 +0530

    Sliding window: Explanation of LC 1358

--- 5_Strings/1358_Number_substrings_containing_all_char.cpp
+++ 5_Strings/1358_Number_substrings_containing_all_char.cpp
@@ -1,5 +1,20 @@
 
+/*
+steps:
 
+1. if a substring contains atleast one 'a', 'b' and 'c' (valid substring) then all its
+extensions will also be valid substring.
+2. suppose substring [l..r] is valid, then its extensions are
+    - [l..r+1], [l..r+1] ... [l..n-1]
+    - so total valid substrings are from r to n-1 i.e n-r
+3. so now instead of counting all valid substrings one by one,
+we find the smallest valid substring and then count all extensions of that once as n-r.
+4. so to find the smallest valid substring we use sliding window(two pointers).
+    - we have left and right pointer
+    - we expand window until the substring is not valid
+    - once we get a valid substring we add n-r to answer, then shrink the window by shifting the left pointer
+    - then again we find the valid window and repeat the process.
+*/
 class Solution {
 public:
     int numberOfSubstrings(string s) {
